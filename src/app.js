0d780cc2c441c803d333f6bf9244444ead9acb8b
Xyzin503 <xyzin503@gmail.com>
2022-04-27 15:00:49 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,3 +1,20 @@
 export class App {
-  message = 'Hello World!';
+  constructor() {
+    this.header = 'Todos';
+    this.todos = [];
+    this.todoDesc = '';
+  }
+  addTodo() {
+    this.todos.push({
+      description: this.todoDesc,
+      done: false
+
+    });
+  }
+
+  delTodo(todo) {
+    let index = this.todo.indexOf(todos);
+    todos.pop(index);
+  }
 }
+
